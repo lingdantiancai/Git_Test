a61db68119949798eef554a300c0fbc99200acbc
TED <yygao9-c@my.cityu.edu.hk>
2026-04-06 21:20:41 +0800

    This is another test

--- line_plot.py
+++ line_plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 plt.rcParams["font.sans-serif"] = ["SimHei", "Microsoft YaHei", "DejaVu Sans"]
 plt.rcParams["axes.unicode_minus"] = False
 
-plt.plot([0, 1, 2, 3, 4], [0, 2, 1, 3, 2])
+plt.plot([0, 1, 2, 3, 4], [0, 5, 1, 3, 2])
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("折线图")
